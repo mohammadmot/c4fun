c033740229c89941de024cf4e68624cf8e0894cd
root <mohammadmot@gmail.com>
2022-05-28 16:37:05 +0000

    update cout with std::

--- 1.cpp
+++ 1.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
-int main()
+int main(void)
 {
-   cout << "hello world.";
-   cout << "This changed by kioumrs" < endl;
-   cout << "This line is added by kioumars for second times";
+   std::cout << "hello world.\r\n";
+   std::cout << "This changed by kioumrs" << std::endl;
+   std::cout << "This line is added by kioumars for second times" << std::endl;
+
    return 0;
 }
