88948ba4c20e9731db75a84ab987d5b8252065ea
kbabakhani <kioumarsbabakhani@gmail.com>
2022-05-31 08:21:56 +0000

    New Line

--- 2.cpp
+++ 2.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 
-int main() {
+int main()
+{
     std::ofstream out("/tmp/demo_file_deleteme");
     out << "write to file hello\r\n";
     out << "next line.\r\n";
--- 3.cpp
+++ 3.cpp
@@ -3,18 +3,21 @@ using namespace std;
 
 int main()
 {
-    variant<int, float> v, w;
-v = 12;
-int i = get<int>(v);
-w = get<int>(v);
-w = get<0>(v); // same effect as the previous line
-w = v; // same effect as the previous line
+  variant<int, float> v, w;
+  v = 12;
+  int i = get<int>(v);
+  w = get<int>(v);
+  w = get<0>(v); // same effect as the previous line
+  w = v;         // same effect as the previous line
 
-get<double>(v); // ill formed
-get<3>(v); // ill formed
+  get<double>(v); // ill formed
+  get<3>(v);      // ill forme
 
-try {
-  get<float>(w); // will throw.
-}
-catch (bad_variant_access&) {}
+  try
+  {
+    get<float>(w); // will throw.
+  }
+  catch (bad_variant_access &)
+  {
+  }
 }
